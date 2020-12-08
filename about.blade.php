adf8374775ec8d803421ba95d0b67e4697ac693f
syarizalsam <69450683+syarizalsam@users.noreply.github.com>
2020-12-08 23:57:47 +0800

    Create about.blade.php

---
+++ about.blade.php
@@ -0,0 +1,13 @@
+@extends('subview/master')
+
+@section('title', 'About the web application')
+
+@section('content')
+
+<h1>E-book sharing application</h1>
+
+<p>If you love books but are running low on shelf space, this application provide a wonderful alternative. 
+They’re conveniently portable, don’t take up space, and can be enjoyed from anywhere 
+(not to mention all the trees you’ll save each year).<p>
+
+@endsection
