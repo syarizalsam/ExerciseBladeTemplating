87935bd2a6efd9b0fb8dbaf7a324bc750894d72b
syarizalsam <69450683+syarizalsam@users.noreply.github.com>
2020-12-09 22:23:53 +0800

    Update list.blade.php

--- list.blade.php
+++ list.blade.php
@@ -7,18 +7,18 @@
 <h1>List of e-books available for sharing: </h1>
     <h2>E-book #1</h2>
     <ul>
-        <li>Title: {{$title1}}<li>
-        <li>ISBN: {{$isbn1}}<li>
-        <li>Category: {{$bookCategory1}}<li>
-        <li>No. of Pages: {{$noPages1}}<li>
+        <li>Title: {{$title1}}
+        <li>ISBN: {{$isbn1}}
+        <li>Category: {{$bookCategory1}}
+        <li>No. of Pages: {{$noPages1}}
     </ul>
     
     <h2>E-Book #2</h2>
     <ul>
-        <li>Title: {{$title2}}<li>
-        <li>ISBN: {{$isbn2}}<li>
-        <li>Category: {{$bookCategory2}}<li>
-        <li>No. of Pages: {{$noPages2}}<li>
+        <li>Title: {{$title2}}
+        <li>ISBN: {{$isbn2}}
+        <li>Category: {{$bookCategory2}}
+        <li>No. of Pages: {{$noPages2}}
     </ul>
 
 @endsection
